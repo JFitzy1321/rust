56033b10f9c00e11eaacd67af598e859005b1ec5
Joe Fitzgibbons <joe.fitzgibbons@ruralsourcing.com>
2020-04-27 17:00:27 -0600

    wrapping up guessing game

--- rust_book_tutorials/src/guessing_game.rs
+++ rust_book_tutorials/src/guessing_game.rs
@@ -1,12 +1,44 @@
+use rand::Rng;
+use std::cmp::Ordering;
+
 pub fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
 
-    let mut guess = String::new();
+    let secret_num: u32 = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret_num);
 
-    std::io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("Please input your guess.");
 
-    println!("You guessed {}", guess);
+        let mut guess = String::new();
+
+        std::io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess = guess.trim();
+
+        if ["quit", "exit"].contains(&guess.to_lowercase().as_ref()) {
+            println!("Quitting game!");
+            break;
+        }
+        let guess: u32 = match guess.parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a whole number!");
+                continue;
+            }
+        };
+
+        println!("You guessed {}", guess);
+
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }

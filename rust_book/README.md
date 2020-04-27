26c6c77a810c3f17b88c88da19f41d98ba7cd396
Joe Fitzgibbons <joe.fitzgibbons@ruralsourcing.com>
2020-04-27 15:49:34 -0600

    Starting Rust book over while waiting around for others

--- rust_book/README.md
+++
@@ -1,7 +0,0 @@
-# rust_book
-
-repo for following rust book
-
-current page: Storing UTF-8 Encoded Text
-
-Windows Path: file:///C:/Users/joe.fitzgibbons/.rustup/toolchains/stable-x86_64-pc-windows-msvc/share/doc/rust/html/book/ch08-02-strings.html
--- rust_book/src/main.rs
+++ rust_book/src/main.rs
@@ -1,29 +1,3 @@
-#![allow(dead_code)]
-
-// mod enum_examples;
-use std::error::Error;
-
-mod lib;
-
-// use lib::control_flow;
-// use lib::fibonacci;
-// use lib::functions;
-// use lib::guessing_game;
-// use lib::if_let;
-// use lib::matching_examples;
-// use lib::options_examples;
-// use lib::ownership;
-// use lib::slice_example;
-// use lib::struct_examples;
-// use lib::variables;
-use lib::collections::vector_examples;
-
-fn main() -> Result<(), Box<dyn Error>> {
-    // enum_examples::main();
-    // enum_examples::main();
-    // control_flow::main();
-    // matching_examples::main();
-    // if_let::main();
-    vector_examples::main();
-    Ok(())
+fn main() {
+    println!("Hello, world!");
 }

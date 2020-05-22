4f3df30413ddb775480e768a9334bbeba7f1a170
Joe Fitzgibbons <joe.fitzgibbons@ruralsourcing.com>
2020-05-22 15:17:13 -0600

    created recursive fibonacci nth number calculator in Rust

---
+++ fibonacci/src/main.rs
@@ -0,0 +1,31 @@
+use std::io::{stdin, stdout, Write};
+
+fn main() {
+    print!("Please enter a number: ");
+    let _ = stdout().flush();
+
+    let mut input_num = String::new();
+    stdin()
+        .read_line(&mut input_num)
+        .expect("Something went wrong reading you input!");
+    let input_num: u32 = input_num
+        .trim()
+        .parse()
+        .expect("Please enter a positive whole number!");
+
+    println!(
+        "The {} number of the Fibonacci Sequence is: {}",
+        input_num,
+        fib(&input_num)
+    );
+}
+
+fn fib(input: &u32) -> u32 {
+    // Get the value of input by dereference
+    if *input == 0 || *input == 1 {
+        *input
+    } else {
+        // Need to make nth a reference again after calculation
+        fib(&(*input - 1)) + fib(&(*input - 2))
+    }
+}
--- the-rust-book/src/main.rs
+++ the-rust-book/src/main.rs
@@ -1,16 +1,7 @@
-#![allow(dead_code)]
-
 fn main() {
     // println!("Hello, cargo! Again...");
     // guessing_game::main();
-    // println!("Chapter 3: Variables!");
-    // chapter_3::variables();
-
-    // println!("Chapter 3: Shadowing!");
-    // chapter_3::shadowing();
-
-    println!("Chapter 3: Array indexing");
-    chapter_3::out_of_bounds_array();
+    chapter_3::main();
 }
 
 // // Chapter 2 : Guessing Game
@@ -61,25 +52,64 @@ fn main() {
 
 // Common Programming Concepts
 mod chapter_3 {
-    pub fn variables() {
-        let x = 5;
-        println!("X is: {}", x);
-        let x = 6;
-        println!("X is now {} after shadowing!", x);
-        const MAX_INT: u32 = 100_000;
+    pub fn main() {
+        // println!("Chapter 3: Variables!");
+        // variables();
+
+        // println!("Chapter 3: Shadowing!");
+        // shadowing();
+
+        // println!("Chapter 3: Array indexing");
+        // out_of_bounds_array();
+        // let num = 43;
+        // let num_type = ternary_num_type(&num);
+        // println!("Number is {}", num_type);
+        for_loop_iterator();
+        for_range();
+    }
+    // pub fn variables() {
+    //     let x = 5;
+    //     println!("X is: {}", x);
+    //     let x = 6;
+    //     println!("X is now {} after shadowing!", x);
+    //     const MAX_INT: u32 = 100_000;
+    // }
+
+    // pub fn shadowing() {
+    //     let text = "I am some text";
+    //     println!("{}", text);
+    //     let text = 32_876;
+    //     println!("{}", text);
+    // }
+
+    // pub fn out_of_bounds_array() {
+    //     let num_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    //     let index = 20;
+
+    //     println!("The {} element is: {}", index, num_array[index]);
+    // }
+
+    // pub fn ternary_num_type<'a>(num: &'a u32) -> &'a str {
+    //     // how todo a ternary operator in rust
+    //     if num % 2 == 0 {
+    //         "even"
+    //     } else {
+    //         "odd "
+    //     }
+    // }
+
+    fn for_loop_iterator() {
+        let a = [10, 20, 30, 40, 50];
+
+        for elem in a.iter() {
+            println!("{}", elem);
+        }
     }
 
-    pub fn shadowing() {
-        let text = "I am some text";
-        println!("{}", text);
-        let text = 32_876;
-        println!("{}", text);
-    }
-
-    pub fn out_of_bounds_array() {
-        let num_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        let index = 20;
-
-        println!("The {} element is: {}", index, num_array[index]);
+    fn for_range() {
+        for num in (1..5).rev() {
+            println!("{}!", num);
+        }
+        println!("BLAST OFF!!!");
     }
 }

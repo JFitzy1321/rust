e851e240f1ea37d6cb1ddf41831c1c3c3ae2676a
Joe Fitzgibbons <jfitzy1321@protonmail.com>
2020-06-27 15:26:21 -0600

    attempting to understand lifetimes

---
+++ sandbox/strsplit/README.md
@@ -0,0 +1,7 @@
+# StrSplit Repository: Not my own!
+
+Following Jon Gjengset's Tutorial on lifetimes
+
+Source: https://www.youtube.com/watch?v=rAl-9HwD858
+
+Left-off Timestamp: https://youtu.be/rAl-9HwD858?t=3124
---
+++ sandbox/strsplit/src/lib.rs
@@ -0,0 +1,42 @@
+#[derive(Debug)]
+pub struct StrSplit<'a> {
+    remainder: &'a str,
+    delimiter: &'a str,
+}
+
+impl<'a> StrSplit<'a> {
+    pub fn new(haystack: &'a str, delimiter: &'a str) -> Self {
+        Self {
+            remainder: haystack,
+            delimiter,
+        }
+    }
+    pub fn on_whitespace(haystack: &'a str) -> Self {
+        Self::new(haystack, &" ")
+    }
+}
+
+impl<'a> Iterator for StrSplit<'a> {
+    type Item = &'a str;
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(next_delim) = self.remainder.find(self.delimiter) {
+            let until_delimiter = &self.remainder[..next_delim];
+            self.remainder = &self.remainder[(next_delim + self.delimiter.len())..];
+            Some(until_delimiter)
+        } else if self.remainder.is_empty() {
+            None
+        } else {
+            let rest = self.remainder;
+            self.remainder = "";
+            Some(rest)
+        }
+    }
+}
+
+#[test]
+fn it_works() {
+    let haystack = "a b c d e";
+    let letters: Vec<_> = StrSplit::new(haystack, " ").collect();
+    let expected = vec!["a", "b", "c", "d", "e"];
+    assert_eq!(letters, expected);
+}

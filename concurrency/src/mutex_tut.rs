aa341dc9575f60902dbfac5dc25d671ac3b270a6
Joe Fitzgibbons <jfitzy1321@protonmail.com>
2020-06-30 12:15:50 -0600

    cleaned up dead code and moved things arounds

--- concurrency/src/mutex_tut.rs
+++
@@ -1,9 +0,0 @@
-use std::sync::Mutex;
-
-fn main() {
-    //..
-}
-fn sync_inc(counter: &Mutex<i32>) {
-    let mut data: Guard<i32> = counter.lock();
-    *data += 1;
-} // unlock once Guard and Mutext falls out of scopre here
--- rust_by_example/README.md
+++
@@ -1,4 +0,0 @@
-# rust_by_example
-repository for following the rust by example book
-
-Current Page in "Rust By Example" book: "Formatted Print"

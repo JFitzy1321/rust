514eb1426ba736a31081ceed35bb2a8c82f693ea
Joe Fitzgibbons <jfitzy1321@protonmail.com>
2020-07-24 19:13:41 -0600

    stuff is update

---
+++ book-code/src/chapter_7.rs
@@ -0,0 +1,5 @@
+use crate::reverse_int;
+
+pub fn main() {
+    println!("Hello From a submodule in Chapter 7!");
+}
--- book-code/src/main.rs
+++ book-code/src/main.rs
@@ -15,13 +15,42 @@
 //     let dime: Coin = Dime;
 // }
 
-mod chapter_6;
-use chapter_6::{Coin, UsState};
+// // mod chapter_6;
+// // use chapter_6::{Coin, UsState};
 
-fn main() {
-    let quarter = Coin::Quarter(UsState::NewYork);
-    match quarter {
-        Coin::Quarter(state) => println!(" I have a quarter from {:?}", state),
-        _ => println!("I have a {} cent coin!", quarter.value()),
+// fn main() {
+//     let quarter = Coin::Quarter(UsState::NewYork);
+//     match quarter {
+//         Coin::Quarter(state) => println!(" I have a quarter from {:?}", state),
+//         _ => println!("I have a {} cent coin!", quarter.value()),
+//     }
+
+//     let _ = reverse_int(123);
+// }
+
+extern crate itertools;
+use itertools::Itertools;
+
+pub fn main() {
+    let x = descending_ints(123);
+}
+
+pub fn descending_ints(x: u64) -> u64 {
+    x.to_string()
+        .chars()
+        .sorted()
+        .rev()
+        .collect::<String>()
+        .parse::<u64>()
+        .unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::descending_ints;
+
+    #[test]
+    fn reverse_int_works() {
+        assert_eq!(4321, descending_ints(1234));
     }
 }

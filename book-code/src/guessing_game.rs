972ce742b515d2ebf24e64ed82fddad4f49514fa
Joe Fitzgibbons <jfitzy1321@protonmail.com>
2020-11-27 01:24:31 -0700

    rewrote the guessing game again

--- book-code/src/guessing_game.rs
+++ book-code/src/guessing_game.rs
@@ -1,15 +1,41 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 pub fn main() {
     println!("Number Guessing Game!");
 
-    println!("Please enter a number.");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    let mut guess = String::new();
+    loop {
+        println!("Please enter a number.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to readline!");
+        let mut guess = String::new();
 
-    println!("You guessed: {}", guess);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to readline!");
+
+        let guess = guess.trim();
+
+        if guess.trim().eq("show") {
+            println!("The secret number is: {}", secret_number);
+        }
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(t) => t,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }

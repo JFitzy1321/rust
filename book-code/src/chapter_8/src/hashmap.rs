a9becdfff5519edc70070ac2729df745d6f5b4bb
Joe Fitzgibbons <joe.fitzgibbons@ruralsourcing.com>
2020-11-30 18:35:35 -0700

    Moved code around and worked through HashMap Section in Chapter 8

--- book-code/src/chapter_8/src/hashmap.rs
+++
@@ -1,8 +0,0 @@
-use std::collections::HashMap;
-
-pub fn main() {
-    let mut scores = HashMap::new();
-
-    scores.insert("Blue".to_string(), 10);
-    scores.insert("Yellow".to_string(), 50);
-}

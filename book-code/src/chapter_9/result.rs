28db95fb71b9ff388d431a5f013d0386bcd17bde
Joe Fitzgibbons <joe.fitzgibbons@ruralsourcing.com>
2020-12-17 16:58:38 -0700

    finally finished chapter 9 of rust book

--- book-code/src/chapter_9/result.rs
+++ book-code/src/chapter_9/result.rs
@@ -1,18 +1,53 @@
 use std::fs::File;
-use std::io::ErrorKind;
+use std::io;
+use std::io::Read;
 
-pub fn main() {
-    let f = File::open("hello.txt");
-    let f = match f {
-        Ok(file) => file,
-        Err(e) => match e.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("Problem creating the file: {:?}", e),
-            },
-            other_error => {
-                panic!("Problem opening the file: {:?}", other_error)
-            }
-        },
-    };
+// pub fn main() {
+//     // let file = File::open("hello.txt");
+//     // let _ = match f {
+//     // Ok(file) => file,
+//     //     Err(e) => match e.kind() {
+//     //         ErrorKind::NotFound => match File::create("hello.txt") {
+//     //             Ok(fc) => fc,
+//     //             Err(e) => panic!("Problem creating the file: {:?}", e),
+//     //         },
+//     //         other_error => {
+//     //             panic!("Problem opening the file: {:?}", other_error)
+//     //         }
+//     //     },
+//     // };
+//     let _ = File::open("hello.txt").unwrap_or_else(|error| {
+//         if error.kind() == ErrorKind::NotFound {
+//             File::create("hello.txt").unwrap_or_else(|error| {
+//                 panic!("Problem creating the file: {:?}", error);
+//             })
+//         } else {
+//             panic!("Problem opening the file: {:?}", error);
+//         }
+//     });
+// }
+
+pub fn main() -> Result<String, io::Error> {
+    // let f = File::open("hello.txt");
+
+    // let mut f = match f {
+    //     Ok(file) => file,
+    //     Err(e) => return Err(e),
+    // };
+
+    // let mut s = String::new();
+
+    // match f.read_to_string(&mut s) {
+    //     Ok(_) => Ok(s),
+    //     Err(e) => Err(e),
+    // }
+
+    // let mut file = File::open("hello.txt")?;
+    // let mut s = String::new();
+    // file.read_to_string(&mut s)?;
+    // Ok(s)
+    let mut s = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
 }
--- book-code/src/main.rs
+++ book-code/src/main.rs
@@ -1,3 +1,4 @@
+#[allow(dead_code)]
 // mod guessing_game;
 mod chapter_8;
 mod chapter_9;
@@ -10,5 +11,5 @@ fn main() {
     // chapter_8::hashmap::main();
 
     //chapter_9::panic::main();
-    chapter_9::result::main();
+    let _ = chapter_9::result::main();
 }

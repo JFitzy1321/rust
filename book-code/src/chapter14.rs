c2bbb639b97fce396f2f03bae417b2d3f26a7034
Joe Fitzgibbons <joe.fitzgibbons@ruralsourcing.com>
2021-02-23 15:35:10 -0700

    forgot to add new file

---
+++ book-code/src/chapter14.rs
@@ -0,0 +1,18 @@
+//! # Chapter 14, Cargo
+//!
+//! `chapter14` is the example code from the rust book
+//!
+
+/// Adds one to the number given
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = my_crate::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}

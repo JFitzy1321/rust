08ad784ce70327731d8264a87aa951fe3645e7a3
Joe Fitzgibbons <joe.fitzgibbons@ruralsourcing.com>
2021-06-08 18:09:16 -0600

    Finished chapter 17 of rust book

--- book-code/src/chapter17.rs
+++ book-code/src/chapter17.rs
@@ -1,6 +1,8 @@
 // OOP in Rust
 pub fn main() {
     trait_objects::main();
+
+    oop_design::main();
 }
 pub struct AveragedCollection {
     list: Vec<i32>,
@@ -40,6 +42,10 @@ mod trait_objects {
 
     // Using 'Box<dyn trait-name>' instead of a trait bound
     // allows for multiple types to used
+
+    // Btw, Trait Objects must be 'Object Safe':
+    // - The return type isn’t Self.
+    // - There are no generic type parameters.
     pub struct Screen {
         // components could hold buttons, ListViews, Scrollbars, text, etc.
         pub components: Vec<Box<dyn Draw>>,
@@ -98,5 +104,197 @@ mod trait_objects {
         };
 
         screen.run();
+    }
+}
+
+mod oop_design {
+
+    // We can do better!
+
+    // mod blog {
+    //     trait State {
+    //         fn request_review(self: Box<Self>) -> Box<dyn State>;
+    //         fn approve(self: Box<Self>) -> Box<dyn State>;
+    //         fn content<'a>(&self, _post: &'a Post) -> &'a str {
+    //             ""
+    //         }
+    //         fn reject(self: Box<Self>) -> Box<dyn State>;
+    //     }
+
+    //     struct Draft {}
+
+    //     impl State for Draft {
+    //         fn request_review(self: Box<Self>) -> Box<dyn State> {
+    //             Box::new(PendingReview { approvals: 0 })
+    //         }
+
+    //         fn approve(self: Box<Self>) -> Box<(dyn State + 'static)> {
+    //             self
+    //         }
+
+    //         fn reject(self: Box<Self>) -> Box<dyn State> {
+    //             self
+    //         }
+    //     }
+
+    //     struct PendingReview {
+    //         approvals: u8,
+    //     }
+
+    //     impl State for PendingReview {
+    //         fn request_review(self: Box<Self>) -> Box<dyn State> {
+    //             self
+    //         }
+
+    //         fn approve(self: Box<Self>) -> Box<dyn State> {
+    //             if self.approvals < 2 {
+    //                 let state_plus1 = PendingReview {
+    //                     approvals: self.approvals + 1,
+    //                 };
+    //                 Box::new(state_plus1)
+    //             } else {
+    //                 Box::new(Published {})
+    //             }
+    //         }
+
+    //         fn reject(self: Box<Self>) -> Box<dyn State> {
+    //             Box::new(Draft {})
+    //         }
+    //     }
+
+    //     struct Published {}
+
+    //     impl State for Published {
+    //         fn request_review(self: Box<Self>) -> Box<dyn State> {
+    //             self
+    //         }
+
+    //         fn approve(self: Box<Self>) -> Box<dyn State> {
+    //             self
+    //         }
+
+    //         fn content<'a>(&self, post: &'a Post) -> &'a str {
+    //             &post.content
+    //         }
+
+    //         fn reject(self: Box<Self>) -> Box<dyn State> {
+    //             self
+    //         }
+    //     }
+
+    //     pub struct Post {
+    //         state: Option<Box<dyn State>>,
+    //         content: String,
+    //     }
+
+    //     impl Post {
+    //         pub fn new() -> Self {
+    //             Self {
+    //                 state: Some(Box::new(Draft {})),
+    //                 content: String::new(),
+    //             }
+    //         }
+
+    //         pub fn add_text<T: AsRef<str>>(&mut self, text: T) {
+    //             self.content.push_str(text.as_ref());
+    //         }
+
+    //         pub fn content(&self) -> &str {
+    //             self.state.as_ref().unwrap().content(self)
+    //         }
+
+    //         pub fn request_review(&mut self) {
+    //             if let Some(s) = self.state.take() {
+    //                 self.state = Some(s.request_review())
+    //             }
+    //         }
+
+    //         pub fn approve(&mut self) {
+    //             if let Some(s) = self.state.take() {
+    //                 self.state = Some(s.approve());
+    //             }
+    //         }
+    //     }
+    // }
+
+    // use blog::Post;
+
+    // pub fn main() {
+    //     let mut post = Post::new();
+    //     let post_text = "I ate a salad for lunch today".to_string();
+    //     post.add_text(post_text.clone());
+    //     assert_eq!("", post.content());
+
+    //     post.request_review();
+    //     assert_eq!("", post.content());
+
+    //     post.approve();
+    //     assert_eq!(&post_text, post.content());
+    // }
+
+    mod blog {
+        pub struct Post {
+            content: String,
+        }
+
+        pub struct DraftPost {
+            content: String,
+        }
+
+        pub struct PendingReviewPost {
+            content: String,
+        }
+
+        impl Post {
+            pub fn new() -> DraftPost {
+                DraftPost {
+                    content: String::new(),
+                }
+            }
+
+            pub fn content(&self) -> &str {
+                &self.content
+            }
+        }
+
+        impl DraftPost {
+            pub fn add_text(&mut self, text: &str) {
+                self.content.push_str(text);
+            }
+
+            pub fn request_review(self) -> PendingReviewPost {
+                PendingReviewPost {
+                    content: self.content,
+                }
+            }
+        }
+
+        impl PendingReviewPost {
+            pub fn approve(self) -> Post {
+                Post {
+                    content: self.content,
+                }
+            }
+
+            pub fn reject(self) -> DraftPost {
+                DraftPost {
+                    content: self.content,
+                }
+            }
+        }
+    }
+
+    use blog::Post;
+
+    pub fn main() {
+        let mut post = Post::new();
+
+        post.add_text("I ate a salad for lunch today");
+
+        let post = post.request_review();
+
+        let post = post.approve();
+
+        assert_eq!("I ate a salad for lunch today", post.content());
     }
 }

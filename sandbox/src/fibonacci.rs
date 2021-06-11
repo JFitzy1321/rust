8b2291e1c22e9f078df9e89aa18254f64ee80760
Joe Fitzgibbons <joe.fitzgibbons@ruralsourcing.com>
2021-06-11 07:52:54 -0600

    worked on fibonacci code a little

--- sandbox/src/fibonacci.rs
+++ sandbox/src/fibonacci.rs
@@ -1,37 +1,62 @@
-use std::io::{stdin, stdout, Write};
+use std::{
+    io::{stdin, stdout, Write},
+    process::exit,
+    thread::sleep,
+    time::Duration,
+};
 
-fn main() {
-    print!("Please enter a positive whole number: ");
-    let _ = stdout().flush();
+pub fn main() {
+    println!("Fibonacci Sequence Calculator, written in Rust!");
+    println!("This app will give you the fibonacci number at a given point in the sequence.");
 
-    let mut input_num = String::new();
-    if let Err(err) = stdin().read_line(&mut input_num) {
-        eprintln!("error: Something went wrong parsing your input!\n{:?}", err);
-        std::process::exit(1);
-    }
+    let valid_continues: Vec<&str> = vec!["y", "Y", "yes", "Yes"];
 
-    let input_num: u32 = match input_num.trim().parse() {
-        Ok(n) => n,
-        Err(err) => {
-            eprintln!("error: Please enter a positive whole number!\n{:?}", err);
-            std::process::exit(1);
+    loop {
+        sleep(Duration::from_millis(500));
+
+        // using print! and flush to have input on the same line as output
+        // I think this looks better, but it doesn't really matter
+        print!("\nPlease enter a positive whole number: ");
+        let _ = stdout().flush();
+
+        let mut input_num = String::new();
+        stdin()
+            .read_line(&mut input_num)
+            .expect("error: Something went wrong parsing your input!");
+
+        let input_num: u32 = input_num
+            .trim()
+            .parse()
+            .expect("error: Please enter a positive whole number!");
+
+        println!(
+            "The {} number of the Fibonacci Sequence is: {}",
+            input_num,
+            fib(&input_num)
+        );
+
+        print!("\nWould you like to calculate another number? (y/n) : ");
+        let _ = stdout().flush();
+
+        let mut continue_input = String::new();
+        let _ = stdin()
+            .read_line(&mut continue_input)
+            .expect("error: Something went wrong reading your answer!");
+
+        if !(valid_continues.contains(&continue_input.trim())) {
+            println!("Goodbye");
+            exit(0);
         }
-    };
-
-    println!(
-        "The {} number of the Fibonacci Sequence is: {}",
-        input_num,
-        fib(&input_num)
-    );
+    }
 }
 
+// TODO: How can I calculate the actual sequence?
+
 fn fib(input: &u32) -> u32 {
-    let num = *input;
-    // Get the value of input by dereference
-    if num == 0 || num == 1 {
-        num
+    if *input == 0 || *input == 1 {
+        *input
     } else {
-        // Need to make nth a reference again after calculation
-        fib(&(num - 1)) + fib(&(num - 2))
+        // Need to make nth number a reference again after calculation
+        fib(&(*input - 1)) + fib(&(*input - 2))
     }
 }

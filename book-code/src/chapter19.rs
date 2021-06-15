9f4bea3295510b65c25338714dbd2f116c97c796
langnostic <langnostic1@pm.me>
2021-06-14 20:24:46 -0600

    Completed unsafe rust section of chapter 19

--- book-code/src/chapter19.rs
+++ book-code/src/chapter19.rs
@@ -10,7 +10,7 @@ mod unsafe_rust {
     // - Call unsafe functions or methods
     // - Implement unsafe traits
     // - Access fields of `union`s (wtf is a union in rust)
-    // It's also need for FFRI (language boundries) and accessing hardware
+    // It's also need for FFI (Foreign Function Interface) and accessing hardware
     // Because these things are 'outside' the scope of the compiler and borrow checker,
     // rust cannot guarantee saftey with this operations
     pub fn main() {
@@ -23,10 +23,46 @@ mod unsafe_rust {
         unsafe {
             println!("r1 is: {}", *r1);
             println!("r2 is: {}", *r2);
+
+            dangerous();
+
+            println!("Absolute value of -3, according to C Library: {}", abs(-3));
         }
 
         // raw pointer to arbitrary memory
         // let address = 0x012345usize;
         // let r = address as *const i32;
     }
+
+    unsafe fn dangerous() {}
+
+    use std::slice;
+
+    fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+        let len = slice.len();
+        let ptr = slice.as_mut_ptr();
+
+        assert!(mid <= len);
+
+        unsafe {
+            (
+                slice::from_raw_parts_mut(ptr, mid),
+                slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+            )
+        }
+    }
+
+    // Call a C ABI
+    extern "C" {
+        fn abs(input: i32) -> i32;
+    }
+
+    // write a funcion to be used by c
+    // pub extern "C" fn call_from_c() {
+    //     println!("Just called a Rust function from C!");
+    // }
+
+    unsafe trait Foo {}
+
+    unsafe impl Foor for i32 {}
 }

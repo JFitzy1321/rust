4e4f1fffb789752ac5d37d2d1a07066448a9d224
Joe Fitzgibbons <32967490+langnostic@users.noreply.github.com>
2021-06-23 10:08:53 -0600

    added phi equation

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,5 +1,12 @@
 mod fibonacci;
 
 fn main() {
+    calc_phi();
     fibonacci::main();
 }
+
+fn calc_phi() {
+    let phi = (1.0 + (5.0f64).sqrt()) / 2.0;
+    println!("PHI calculated in Rust: {}", phi);
+}
+
